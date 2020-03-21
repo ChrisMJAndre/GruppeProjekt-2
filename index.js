0657d53b81bd49729071581168de10c0acb835e2
Niklas Lykke-Bøndergaard <nilb107@hotmail.com>
2020-03-21 14:43:08 +0100

    Opdateret alt med ejs
    Så kan vi nemmere komme igang med arbejde med database og server forhåbentligt

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
 const express = require('express');
+const http = require('http')
+const reload = require('reload')
 const path = require('path');
 const app = new express();
 const ejs = require('ejs');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    index.ejs'))
     res.render('index');
 })
 app.get('/login', (req, res) => {
-    //   about.ejs'))
     res.render('login');
 })
+app.get('/register', (req, res) => {
+    res.render('register');
+})
 app.get('/student', (req, res) => {
-    //  contact.ejs'))
     res.render('student');
 })
 app.get('/admin', (req, res) => {
-    // post.ejs'))
     res.render('admin');
 })
 app.get('/teacher', (req, res) => {
-    //   post.ejs'))
     res.render('teacher');
 })
 app.get('/removeStudent', (req, res) => {
-    // post.ejs'))
     res.render('removeStudent');
 })
 app.get('/removeLecture', (req, res) => {
-    //post.ejs'))
     res.render('removeLecture');
 })
 app.get('/createLecture', (req, res) => {
-    //post.ejs'))
     res.render('createLecture');
 })
-app.use(express.static('public'));
-//app.use(express.static(path.join(__dirname + '/public')));
-//app.use(express.static(__dirname + '../public'));
-app.listen(4000, () => {
-    console.log('App listening on port 4000');
+
+app.get('/deleteStudent', (req, res) => {
+    res.render('deleteStudent');
+})
+app.get('/deleteTeacher', (req, res) => {
+    res.render('deleteTeacher');
+})
+app.get('/deleteLecture', (req, res) => {
+    res.render('deleteLecture');
+})
+app.get('/showStudentInformation', (req, res) => {
+    res.render('showStudentInformation');
+})
+app.get('/showTeacherInformation', (req, res) => {
+    res.render('showTeacherInformation');
 })
 
 
-//Forsøg på Github Desktop
+app.use(express.static('public'));
+const server = http.createServer(app);
+server.listen(4000, () => {
+    console.log('App listening on port 4000');
+})
+reload(app);
--- public/js/teacher.js
+++ public/js/teacher.js
@@ -5,13 +5,13 @@ class User {
         this.password = password;
         this.email = email;
         this.phoneNumber = phoneNumber;
-        this.firstName= firstName;
+        this.firstName = firstName;
         this.lastName = lastName;
     }
 }
 // Sub Class student which extends the user. Meaning: This class gets most of its attributes from the class user, but a few new ones, such as study program and semester - Chris/Josefine
 class Teacher extends User {
-    constructor (userName, password, email, phoneNumber, firstName, lastName, studyProgram, semester) {
+    constructor(userName, password, email, phoneNumber, firstName, lastName, studyProgram, semester) {
         super(userName, password, email, phoneNumber, firstName, lastName);
 
         this.studyProgram = studyProgram;
@@ -30,17 +30,17 @@ var teacherList = [];
 if (localStorage.getItem("Teacher") == null) {
 
 
-// Here we push a new Teacher to our empty array - Chris
-    teacherList.push(new Teacher("Jose","4567","Josefinecolberg@gmail.com","23626206","Josefine","Colberg","Ha(it)","3"));
-    teacherList.push(new Teacher("Ca18ab", "Øl4ever", "ca18ab@student.cbs.com", "45668921", "Calle", "Olsen", "HA(it)","5"));
+    // Here we push a new Teacher to our empty array - Chris
+    teacherList.push(new Teacher("Jose", "4567", "Josefinecolberg@gmail.com", "23626206", "Josefine", "Colberg", "Ha(it)", "3"));
+    teacherList.push(new Teacher("Ca18ab", "Øl4ever", "ca18ab@student.cbs.com", "45668921", "Calle", "Olsen", "HA(it)", "5"));
     teacherList.push(new Teacher("Ølgod", "password", "miøl18a@student.cbs.com", "66698724", "Mikkel", "Ølgod", "HA(jur.)", "4"));
 
 
-// The studentlist is then made into a string using the JSON.stringify command - Chris
+    // The studentlist is then made into a string using the JSON.stringify command - Chris
     var teacherListString = JSON.stringify(teacherList);
 
 
-// Here we set out studentListString in local storage so that we can later retrieve it - Chris
+    // Here we set out studentListString in local storage so that we can later retrieve it - Chris
     localStorage.setItem('Teacher', teacherListString)
 } else {
     teacherList = JSON.parse(localStorage.getItem('Teacher'))
@@ -55,26 +55,26 @@ function validateHhMm(inputField) {
 
     //Tjekker at feltet har en lovlig værdi - Alex
     // ! betyder is true. Hvis der altså er indtastet date i feltet "tid" så fortsætter den.
-    if(!data) {
+    if (!data) {
         return false;
     }
 
     // Splitter teksten ud i to ved kolon. Hvis der ikke bliver 2 elementer, altså time og minut, er datoen formateret forkert. - Alex
     //val[0] indeholder timen og val[1] indeholder minuttet - Alex
     var val = data.split(':');
-    if(val.length != 2) { //Hvis der kommer alt andet end to værdier ud, så er enten ikke et kolon eller mere end et hvilket er forkert. - Alex
+    if (val.length != 2) { //Hvis der kommer alt andet end to værdier ud, så er enten ikke et kolon eller mere end et hvilket er forkert. - Alex
         return false;
     }
-//inspiration til split: https://www.dotnetperls.com/split-js
+    //inspiration til split: https://www.dotnetperls.com/split-js
 
     // Tjekker at tallet er positivt og at antallet af timer angivet ikke er mere end 23
-    if(Math.sign(val[0]) == -1 || val[0] > 23) { //Hvis math.sign = -1 er det et negativt hvilket ikke er korrekt. - Alex
+    if (Math.sign(val[0]) == -1 || val[0] > 23) { //Hvis math.sign = -1 er det et negativt hvilket ikke er korrekt. - Alex
         return false;
     } //Math.sign et library (et library er en samling af functioner) og en af de functioner er noget der kan opfange om tallene er negative eller positive. (udregner om de positive eller negative). -Alex
-//kilde til math.sign: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
+    //kilde til math.sign: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/sign
 
     // Tjekker at tallet er positivt og at antallet af minutter angivet ikke er mere end 59. - Alex
-    if(Math.sign(val[1]) == -1 || val[1] > 59) { //Hvis math.sign = -1 er det et negativt hvilket ikke er korrekt. - Alex
+    if (Math.sign(val[1]) == -1 || val[1] > 59) { //Hvis math.sign = -1 er det et negativt hvilket ikke er korrekt. - Alex
         return false;
     }
     //Retunerer tidspunktet, som er blevet indtastet. - Alex
@@ -87,7 +87,7 @@ function validateCreateLecture() {
         alert("Name must be entered!");
         return false;
     }
-// Tjekker om der er blevet valgt noget andet end standard-værdien ved dropdown. Alerter at der skal vælges noget andet end standard-værdien. - Alex
+    // Tjekker om der er blevet valgt noget andet end standard-værdien ved dropdown. Alerter at der skal vælges noget andet end standard-værdien. - Alex
     if (document.getElementById("studyProgram").value == "Select study program") {
         alert("Study program must be selected!");
         return false;
@@ -104,7 +104,7 @@ function validateCreateLecture() {
         alert("Classroom must be selected!");
         return false;
     }
-// Her tjekkes der om text-feltet er tomt. Hvis ikke så fortsætter den. - Alex
+    // Her tjekkes der om text-feltet er tomt. Hvis ikke så fortsætter den. - Alex
     if (!document.getElementById("lectureName").value) {
         alert("Lecture name must be entered!");
         return false;
@@ -128,7 +128,7 @@ function validateCreateLecture() {
 Vi kunne heller ikke få forelæsning op i local storage, så har i stedet valgt at gøre det sådan her - Nik */
 function createLecture() {
     // Performs validation. -Alex
-// Her trykker man på knappen create lecture og så starter valideringen som ses ovenfor. Så kører den igennem step by step som ses ovenfor. -Alex
+    // Her trykker man på knappen create lecture og så starter valideringen som ses ovenfor. Så kører den igennem step by step som ses ovenfor. -Alex
     if (!validateCreateLecture()) {
         return;
     }
@@ -140,7 +140,7 @@ function createLecture() {
     var day1 = document.getElementById("day").value;
     var time1 = document.getElementById("time").value;
     var comment1 = document.getElementById("comment").value;
-// Her alertes den forelæsning man opretter med informationerne om de hardcodede classrooms fra det andet js dokument - Nik
+    // Her alertes den forelæsning man opretter med informationerne om de hardcodede classrooms fra det andet js dokument - Nik
     alert("Your lecture has been created"
         + "\nTeacher: " + teacher1
         + "\nStudy program: " + studyProgram1
@@ -161,13 +161,15 @@ function removeLecture() {
 function showStudentInformation() {
     var studentinformation = JSON.parse(localStorage.getItem("Student"));
     alert("Student Information:"
-        + "\n name: "  + studentinformation[0].firstName + " " + studentinformation[0].lastName + "\n email: " + studentinformation[0].email
-        + "\n name: "  + studentinformation[1].firstName + " " + studentinformation[1].lastName + "\n email: " + studentinformation[1].email
-        + "\n name: "  + studentinformation[2].firstName + " " + studentinformation[2].lastName + "\n email: " + studentinformation[2].email
-        + "\n name: "  + studentinformation[3].firstName + " " + studentinformation[3].lastName + "\n email: " + studentinformation[3].email
+        + "\n name: " + studentinformation[0].firstName + " " + studentinformation[0].lastName + "\n email: " + studentinformation[0].email
+        + "\n name: " + studentinformation[1].firstName + " " + studentinformation[1].lastName + "\n email: " + studentinformation[1].email
+        + "\n name: " + studentinformation[2].firstName + " " + studentinformation[2].lastName + "\n email: " + studentinformation[2].email
+        + "\n name: " + studentinformation[3].firstName + " " + studentinformation[3].lastName + "\n email: " + studentinformation[3].email
     )
 }
 
+
+//Alt herfra og ned skal slettes, når vi får oprettet databasen, og arbejder heri.
 
 /* Her fik jeg hjælp af min til block og none delen
 Men lad mig forklare. previousHidden er et array, hvor vi senere pusher elementer ind der skal være hidden - Nik */
@@ -184,28 +186,28 @@ function selectStudentsInLectures() {
         alert("No students in this lecture");
         /* Ved de næste tre values blockerer den deres hidden tilstand mens det er den value der er valgt i første dropdown liste
         Derefter pushes de ind i arrayet så de er hidden igen når man vælger en anden value i første dropdown - Nik */
-    }else if (x == "2") {
+    } else if (x == "2") {
         var x = document.getElementById("studentsIn2");
         x.style.display = "block";
         previousHidden.push(x);
         var y = document.getElementById("removeButton2");
         y.style.display = "block";
         previousHidden.push(y);
-    }else if (x == "3") {
+    } else if (x == "3") {
         var x = document.getElementById("studentsIn3");
         x.style.display = "block";
         previousHidden.push(x);
         var y = document.getElementById("removeButton3");
         y.style.display = "block";
         previousHidden.push(y);
-    }else if (x == "4") {
+    } else if (x == "4") {
         var x = document.getElementById("studentsIn4");
         x.style.display = "block";
         previousHidden.push(x);
         var y = document.getElementById("removeButton4");
         y.style.display = "block";
         previousHidden.push(y);
-    }else {
+    } else {
         alert("Select a lecture to show its students");
     }
 }
