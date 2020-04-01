a087edec8c5eaab1a9dcff34f7f8eaa07ed61dee
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-04-01 10:27:23 +0200

    oprettelse til database (postsql)

--- server/test.js
+++ server/test.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
     host: 'localhost',
     port: 5432,
-    user: 'test',
-    database: 'test',
-    password: 'test',
+    user: 'postgres',
+    database: 'StudentTeach',
+    password: 'studentTeach123',
 });
 
 pool.query('SELECT NOW()').then(result => {
