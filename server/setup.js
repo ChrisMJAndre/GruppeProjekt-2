3c7ef43c5eb772679cc72f00e5ea38896a8ce2d0
Niklas Lykke-Bøndergaard <nilb107@hotmail.com>
2020-04-01 12:21:07 +0200

    Måske hvis de bare hedder id

--- server/setup.js
+++ server/setup.js
@@ -1,44 +1,44 @@
 const pool = require('./db');
 pool.query(`
     CREATE TABLE Classroom(
-        Classroomid int,
+        id int,
         size INT NOT NULL,
         location text NOT NULL,
-        PRIMARY KEY(Classroomid));
+        PRIMARY KEY(id));
     
     CREATE TABLE StudyProgramme( 
-        StudyProgrammeid int,
+        id int,
         title text NOT NULL,
-        PRIMARY KEY(StudyProgrammeid));
+        PRIMARY KEY(id));
     
     CREATE TABLE Course(  
-        Courseid int,  
+        id int,  
         title text NOT NULL, 
         semester INT NOT NULL, 
-        PRIMARY KEY(Courseid),
-        FOREIGN KEY (StudyProgrammeid) REFERENCES StudyProgramme(StudyProgrammeid));
+        PRIMARY KEY(id),
+        FOREIGN KEY (id) REFERENCES StudyProgramme(id));
     
     CREATE TABLE User(
-        Userid int,
+        id int,
         firstName text NOT NULL, 
         lastName text NOT NULL, 
         email text NOT NULL, 
         phoneNumber INT NOT NULL, 
         type text NOT NULL,
-        PRIMARY KEY(Userid),
-        FOREIGN KEY (StudyProgrammeid) REFERENCES StudyProgramme(StudyProgrammeid));
+        PRIMARY KEY(id),
+        FOREIGN KEY (id) REFERENCES StudyProgramme(id));
         
     CREATE TABLE Lecture(  
-        Lectureid int,  
+        id int,  
         lectureName text NOT NULL, 
         date date NOT NULL,
         time time NOT NULL,
         comment text NOT NULL,
         listOfStudents char[],
-        PRIMARY KEY(Lectureid),
-        FOREIGN KEY (Courseid) REFERENCES Course(Courseid));
-        FOREIGN KEY (Classroomid) REFERENCES Classroom(Classroomid));
-        FOREIGN KEY (Userid) REFERENCES User(Userid));
+        PRIMARY KEY(id),
+        FOREIGN KEY (id) REFERENCES Course(id));
+        FOREIGN KEY (id) REFERENCES Classroom(id));
+        FOREIGN KEY (id) REFERENCES User(id));
         );
     
         
