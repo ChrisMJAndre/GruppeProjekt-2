b500675c48cde0024cc49af3069a7f49ceb28880
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-04-01 16:45:05 +0200

    Ændringer ved setup med database

--- server/setup.js
+++ server/setup.js
@@ -1,50 +1,12 @@
 const pool = require('./db');
 pool.query(`
-        CREATE TABLE Classroom(
-            id INT PRIMARY KEY,
-            size INT NOT NULL,
-            location text NOT NULL);
 
-        CREATE TABLE StudyProgramme(
-            id INT PRIMARY KEY,
-            title text NOT NULL);
-
-        CREATE TABLE Course(
-            id INT PRIMARY KEY,
-            title text NOT NULL,
-            semester INT NOT NULL,
-            StudyProgramme_id INT REFERENCES StudyProgramme (id));
             
-        CREATE TABLE UserType( 
-            id INT PRIMARY KEY, 
-            title text NOT NULL); 
-
-        CREATE TABLE Users(
-            id INT PRIMARY KEY,
-            firstName text NOT NULL, 
-            lastName text NOT NULL, 
-            email text NOT NULL, 
-            phoneNumber INT NOT NULL,
-            UserType_id INT REFERENCES UserType (id),
-            StudyProgramme_id INT REFERENCES StudyProgramme (id));
             
-        CREATE TABLE Lecture(
-            id INT PRIMARY KEY,
-            lectureName text NOT NULL,
-            date date NOT NULL,
-            time time NOT NULL,
-            comment text NOT NULL,
-            Course_id INT REFERENCES Course (id),
-            Classroom_id INT REFERENCES Classroom (id),
-            Users_id INT REFERENCES Users (id),
-            SignUp_id INT REFERENCES SignUp (id)
-            );
             
-        CREATE TABLE SignUp(
-            id INT PRIMARY KEY, 
-            Users_id INT REFERENCES Users (id),
-            Lecture_id INT REFERENCES Lecture (id)
-            );
+ALTER TABLE SignUp 
+ADD CONSTRAINT FK_Lecture_id FOREIGN KEY (id)
+REFERENCES Lecture (id)
 
         
 `).then(result => {
@@ -52,6 +14,7 @@ pool.query(`
 });
 
 /*
-
+Denne skal insert ind i signup
+ Lecture_id INT REFERENCES Lecture (id));
 
  */
