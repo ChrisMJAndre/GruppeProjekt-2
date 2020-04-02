682d296a4a693ca20787caa0e75e74960c6aa1b6
Niklas Lykke-Bøndergaard <nilb107@hotmail.com>
2020-04-02 14:01:16 +0200

    gimme chris create table ting ting

--- server/db.js
+++ server/db.js
@@ -13,3 +13,14 @@ pool.query('SELECT NOW()').then(result => {
     pool.end()
 });
 
+
+//Fra: https://www.w3jar.com/node-js-login-and-registration-system-with-express-js-and-mysql/
+const mysql = require('mysql2');
+const dbConnection = mysql.createPool({
+    host: 'localhost', // MYSQL HOST NAME
+    user: 'postgres',        // MYSQL USERNAME
+    port: '5432',
+    password: 'studentTeach123',    // MYSQL PASSWORD
+    database: 'Studenteach'      // MYSQL DB NAME
+}).promise();
+module.exports = dbConnection;
