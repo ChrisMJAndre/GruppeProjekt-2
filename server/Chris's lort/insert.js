35ab51473f39fe21cdf7c80e0460d9715fa4c6d9
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-04-17 14:45:10 +0200

    Update insert.js

--- server/Chris's lort/insert.js
+++ server/Chris's lort/insert.js
@@ -12,17 +12,12 @@ pool.query(`
                 ('90', 'Porcelænshaven'),
                 ('50', 'Kilen'); 
     
-    
-    
     /* Insert studyprogrammes into the studyProgramme Table */    
     INSERT INTO studyProgramme (title) 
         VALUES 
                 ('Ha(It.)'),
                 ('Ha(Jur.)'), 
                 ('Ha(Mat.)');
-    
-    
-    
     
     /* Insert courses into the Course Table */
     INSERT INTO Course (title, semester, studyProgramme_id)
@@ -39,20 +34,26 @@ pool.query(`
                 ('Numerisk analyse', '1','3'),
                 ('Videnskabteori', '2','3'); 
 
-
     /* Insert Student into the Student Table, not all Student */
     INSERT INTO Student (firstName, lastName, passWord, email, phoneNumber, StudyProgramme_id)
-            VALUES ('Niklas', 'Lykke-Bondergaard', '1234', 'Nily19ab@student.cbs.dk', '31320232', '1');
-    
-    
-    
-    
+            VALUES 
+                    ('Niklas', 'Lykke-Bondergaard', '1234', 'Nily19ab@student.cbs.dk', '31320232', '1'),
+                    ('Josefine', 'Colberg', '2468', 'Joje19ae@student.cbs.dk', '26236206', '1'),
+                    ('Kasper', 'ThatGuy', '1357', 'Kath19ay@student.cbs.dk','45455267','2'),
+                    ('Jens', 'Jensen', '9876', 'Jeje19ab@student.cbs.dk', '45678901', '2'),
+                    ('Hans', 'Hansen', '4534', 'Haha19ae@student.cbs.dk', '12345678', '3'),
+                    ('Mathias', 'Mathiasen', '9843', 'Mama19at@student.cbs.dk', '95953535', '3');
+            
     /* Insert teacher into the Teacher Table, not all teacher */
     INSERT INTO Teacher (firstName, lastName, passWord, email, phoneNumber, StudyProgramme_id)
-            VALUES ('Christopher M.J.', 'André', '5678', 'Chan19af@student.cbs.dk', '31418551', '2');
-
-
-
+            VALUES 
+                    ('Christopher M.J.', 'André', '5678', 'Chan19af@student.cbs.dk', '31418551', '2'),
+                    ('Alexander', 'Al-Gamour', '1234', 'Alal19ab@student.cbs.dk', '12345678', '1'),
+                    ('Helene', 'Hansen', '4567', 'Hehe15ay@student.cbs.dk', '65436543', '2'),
+                    ('Anders', 'Andersen', '3456', 'Anan17ab@student.cbs.dk', '34563456', '2'), 
+                    ('Simon', 'Jensen', '1278', 'Sije18au@student.cbs.dk', '23235656', '3'),
+                    ('Andreas', 'Rasmussen', '8765', 'Anra17ap@student.cbs.dk', '42424501', '3');
+                    
    /* Insert Lectures into the Lecture Table */
     INSERT INTO Lecture (lectureName, date, time, comment, teacher_id, classroom_id, course_id)
         VALUES 
