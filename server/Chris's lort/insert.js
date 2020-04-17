c2e23d5589f3b6b3f63ad6dc3fb7443aac16cdf1
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-04-17 14:03:48 +0200

    oprette mere, især list of students
    jfj

--- server/Chris's lort/insert.js
+++ server/Chris's lort/insert.js
@@ -3,70 +3,67 @@
 const pool = require('../db');
 pool.query(`
  
-    /* Insert studyprogrammes into the studyProgramme Table */    
-    INSERT INTO studyProgramme (id, title) 
-        VALUES 
-                ('1', 'Ha(It.)'),
-                ('2', 'Ha(Jur.)'), 
-                ('3', 'Ha(Mat.)');
-
-
     /* Insert classrooms into the Classroom Table */
-    INSERT INTO Classroom (id, size, location) 
+    INSERT INTO Classroom (size, location)
+        VALUES
+                ('50', 'Solbjerg Plads'),
+                ('40', 'Flintholm'),
+                ('60', 'Dalgas Have'),
+                ('90', 'Porcelænshaven'),
+                ('50', 'Kilen'); 
+    
+    
+    
+    /* Insert studyprogrammes into the studyProgramme Table */    
+    INSERT INTO studyProgramme (title) 
         VALUES 
-                ('200', '50', 'Solbjerg Plads'),
-                ('201', '40', 'Flintholm'), 
-                ('202', '60', 'Dalgas Have'),
-                ('203', '90', 'Porcelænshaven'),
-                ('204', '50', 'Kilen');   
-        
+                ('Ha(It.)'),
+                ('Ha(Jur.)'), 
+                ('Ha(Mat.)');
+    
+    
+    
     
     /* Insert courses into the Course Table */
-    INSERT INTO Course (id, title, semester, studyProgramme_id)
+    INSERT INTO Course (title, semester, studyProgramme_id)
         VALUES 
-                ('100', 'VØS', '1','1'),
-                ('101', 'Organisation', '1','1'),
-                ('102', 'Programmering', '1','1'),
-                ('103', 'VØS2', '2','1'),
-                ('104', 'IT-Projektledelse', '2','1'),
-                ('105', 'Aftaleret', '1','2'),
-                ('106', 'Markedsret', '2','2'),
-                ('107', 'Forvaltningsret', '2','2'),
-                ('108', 'Erhvervsøkonomi', '1','3'),    
-                ('109', 'Numerisk analyse', '1','3'),
-                ('110', 'Videnskabteori', '2','3');        
-        
-    /* Insert Lectures into the Lecture Table */
-    INSERT INTO Lecture (id, lectureName, date, time, comment, Course_id, Classroom_id, Users_id, SignUp_id)
+                ('VØS', '1','1'),
+                ('Organisation', '1','1'),
+                ('Programmering', '1','1'),
+                ('VØS2', '2','1'),
+                ('IT-Projektledelse', '2','1'),
+                ('Aftaleret', '1','2'),
+                ('Markedsret', '2','2'),
+                ('Forvaltningsret', '2','2'),
+                ('Erhvervsøkonomi', '1','3'),    
+                ('Numerisk analyse', '1','3'),
+                ('Videnskabteori', '2','3'); 
+
+
+    /* Insert Student into the Student Table, not all Student */
+    INSERT INTO Student (firstName, lastName, passWord, email, phoneNumber, StudyProgramme_id)
+            VALUES ('Niklas', 'Lykke-Bondergaard', '1234', 'Nily19ab@student.cbs.dk', '31320232', '1');
+    
+    
+    
+    
+    /* Insert teacher into the Teacher Table, not all teacher */
+    INSERT INTO Teacher (firstName, lastName, passWord, email, phoneNumber, StudyProgramme_id)
+            VALUES ('Christopher M.J.', 'André', '5678', 'Chan19af@student.cbs.dk', '31418551', '2');
+
+
+
+   /* Insert Lectures into the Lecture Table */
+    INSERT INTO Lecture (lectureName, date, time, comment, teacher_id, classroom_id, course_id)
         VALUES 
-                ('600', 'Programmerings hjælp', '2020/02/13', '19:00', 'comment?', '102', '200', '300', '1'), 
-                ('601', 'Organisations hjælp', '2020/02/13', '19:00', 'comment?', '103', '202', '301', '2'), 
-                ('602', 'VØS hjælp', '2020/02/13', '19:00', 'comment?', '100', '200', '303', '0'), 
-                ('603', 'How to be a laywer', '2020/02/13', '19:00', 'comment?', '105', '201', '302', '3'), 
-                ('604', 'Erhvervsmatematik hjælp', '2020/02/13', '19:00', 'comment?', '108', '204', '304', '4');                                                                
+                ('Programmerings hjælp', '2020/02/13', '19:00', 'comment?', '1', '1', '1'), 
+                ('Organisations hjælp', '2020/02/13', '19:00', 'comment?', '1', '3', '2'), 
+                ('VØS hjælp', '2020/02/13', '19:00', 'comment?', '1', '1', '4'), 
+                ('How to be a laywer', '2020/02/13', '19:00', 'comment?', '1', '2', '3'), 
+                ('Erhvervsmatematik hjælp', '2020/02/13', '19:00', 'comment?', '1', '5', '5');
     
-    
-    /* Insert Users into the Users Table, not all users */
-    INSERT INTO Users (id, firstName, lastName, passWord, email, phoneNumber, UserType_id, StudyProgramme_id) 
-        VALUES 
-                ('300', 'Niklas', 'Lykke-Bøndergaard', '1234', 'Nily19ab@student.cbs.dk', '31320232', '1','1')
-    
-    
-    
-        
-     
-        
-        
-    /* Insert signup into the SignUp Table, no hardcoded data, here the table should gather the data on its own */
-    /* TABLE Signup */
-    
-    
-    
-    
-    
-    
-    
-        
+
+
     `).then(result => {
     console.log(error, result);
 });
