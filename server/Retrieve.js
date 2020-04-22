b0b99cf6531e85f7e2e4c69e24842dddea1ac04f
Joseeeh <Joje19ae@student.cbs.dk>
2020-04-22 10:51:24 +0200

    bare nej

--- server/Retrieve.js
+++ server/Retrieve.js
@@ -1,20 +1,18 @@
 //A way to retrieve all the data from the database
 //https://bezkoder.com/node-express-sequelize-postgresql/
 
-// const pg = require('pg');
+exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
 
-module.exports = {
-
-    client.query('SELECT * FROM studyProgramme').then(res => {
-
-        const fields = res.fields.map(field => field.name);
-
-        console.log(fields);
-
-    }).catch(err => {
-        console.log(err.stack);
-    }).finally(() => {
-        client.end()
-    });
-
-}
+    RetrieveController.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
+};
