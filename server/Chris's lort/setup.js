08365b4179711969ece277152a9dc857306e9b17
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-04-27 14:37:16 +0200

    well det virker igen nu, har slettet en masse ihihihi
    well det virker igen nu, har slettet en masse ihihihi

--- server/Chris's lort/setup.js
+++ server/Chris's lort/setup.js
@@ -49,16 +49,7 @@ pool.query(`
         PRIMARY KEY (id),
         CONSTRAINT fk_studyprogramme_id FOREIGN KEY (studyprogramme_id) REFERENCES Studyprogramme(id));
     
-    
-        
-        CREATE TABLE listOfStudents( 
-            id SERIAL,
-            student_id INT,
-            lecture_id INT, 
-            PRIMARY KEY (id),
-            CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES Student(id));
-    
-            CREATE TABLE Lecture(
+    CREATE TABLE Lecture(
         id SERIAL,
         lectureName text NOT NULL,
         date date NOT NULL,
@@ -67,15 +58,21 @@ pool.query(`
         teacher_id INT NOT NULL, 
         classroom_id INT NOT NULL, 
         course_id INT NOT NULL,
-        listOfStudents_id INT,
         PRIMARY KEY (id),
         CONSTRAINT fk_teacher_id FOREIGN KEY (teacher_id) REFERENCES Teacher(id),
         CONSTRAINT fk_classroom_id FOREIGN KEY (classroom_id) REFERENCES Classroom(id),
-        CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES Course(id),
-        CONSTRAINT fk_listOfStudents_id FOREIGN KEY (listOfStudents_id) REFERENCES listOfStudents(id));
+        CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES Course(id));
         
-ALTER TABLE listOfStudents
-    ADD CONSTRAINT fk_lecture_id FOREIGN KEY (lecture_id) REFERENCES Lecture(id);
+        CREATE TABLE listOfStudents( 
+            id SERIAL,
+            student_id INT,
+            lecture_id INT, 
+            PRIMARY KEY (id),
+            CONSTRAINT fk_lecture_id FOREIGN KEY (lecture_id) REFERENCES lecture(id),
+            CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES Student(id));
+    
+            
+        
 
 
    
